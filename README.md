25cd8fc819a011f8cf64496ae9f213153ff77a9e
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-10-12 16:03:59 +0700

    feat: running train pipeline

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PRCMLPipeline
 End-to-End PRC Data Challenge Pipeline
+
+
+# Installation
+pip install "zenml["server"]"
+
+```angular2html
+zenml init
+```
---
+++ steps/clean_data.py
@@ -0,0 +1,23 @@
+from typing import Tuple
+
+import pandas as pd
+from zenml import step
+
+from src.data_cleaning import DataCleaning
+
+
+@step
+def clean_data(
+        trajectory_df: pd.DataFrame,
+        train_df: pd.DataFrame,
+        test_df: pd.DataFrame
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """Cleans the trajectory, train, and test data."""
+    data_cleaning = DataCleaning()
+
+    train_df_cleaned = data_cleaning.clean_train_test(train_df)
+    test_df_cleaned = data_cleaning.clean_train_test(test_df)
+
+    trajectory_df_cleaned = data_cleaning.clean_trajectory(trajectory_df)
+
+    return trajectory_df_cleaned, train_df_cleaned, test_df_cleaned
