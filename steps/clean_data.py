0f10746d87cc8f092d3a6e04c96748992865a2e6
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-10-13 11:26:41 +0700

    chore: added config dependency injection

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,28 +1,51 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import pandas as pd
 from typing_extensions import Annotated
 from zenml import step
 
 from src.data_cleaning import DataCleaning
+from config import Config
 
 
 @step
 def clean_data(
-    trajectory_df: Annotated[pd.DataFrame, "Trajectory df"],
-    train_df: Annotated[pd.DataFrame, "Train df"],
-    test_df: Annotated[pd.DataFrame, "Test df"],
-) -> Tuple[
-    Annotated[pd.DataFrame, "Cleaned trajectory df"],
-    Annotated[pd.DataFrame, "Cleaned train df"],
-    Annotated[pd.DataFrame, "Cleaned test df"],
+        config: Config,
+        data: Union[
+            Tuple[
+                Annotated[pd.DataFrame, "Train df"],
+                Annotated[pd.DataFrame, "Test df"]],
+            Tuple[
+                Annotated[pd.DataFrame, "Trajectory df"],
+                Annotated[pd.DataFrame, "Train df"],
+                Annotated[pd.DataFrame, "Test df"],
+            ],
+        ],
+) -> Union[
+    Tuple[
+        Annotated[pd.DataFrame, "Cleaned train df"],
+        Annotated[pd.DataFrame, "Cleaned test df"],
+    ],
+    Tuple[
+        Annotated[pd.DataFrame, "Cleaned trajectory df"],
+        Annotated[pd.DataFrame, "Cleaned train df"],
+        Annotated[pd.DataFrame, "Cleaned test df"],
+    ],
 ]:
     """Cleans the trajectory, train, and test data."""
     data_cleaning = DataCleaning()
 
+    if config.USE_TRAJECTORY:
+        trajectory_df, train_df, test_df = data
+        trajectory_df_cleaned = data_cleaning.clean_trajectory(trajectory_df)
+    else:
+        train_df, test_df = data
+        trajectory_df_cleaned = None
+
     train_df_cleaned = data_cleaning.clean_train_test(train_df)
     test_df_cleaned = data_cleaning.clean_train_test(test_df)
 
-    trajectory_df_cleaned = data_cleaning.clean_trajectory(trajectory_df)
-
-    return trajectory_df_cleaned, train_df_cleaned, test_df_cleaned
+    if config.USE_TRAJECTORY:
+        return trajectory_df_cleaned, train_df_cleaned, test_df_cleaned
+    else:
+        return train_df_cleaned, test_df_cleaned
--- steps/feature_engineering.py
+++ steps/feature_engineering.py
@@ -1,31 +1,48 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import pandas as pd
 from typing_extensions import Annotated
 from zenml import step
 
+from config import Config
 from src.feature_engineering import FeatureEngineering
 
 
 @step
 def feature_engineering(
-    trajectory_df: Annotated[pd.DataFrame, "Trajectory df"],
-    train_df: Annotated[pd.DataFrame, "Train df"],
-    test_df: Annotated[pd.DataFrame, "Test df"],
+        config: Config,
+        data: Union[
+            Tuple[
+                Annotated[pd.DataFrame, "Cleaned train df"],
+                Annotated[pd.DataFrame, "Cleaned test df"],
+            ],
+            Tuple[
+                Annotated[pd.DataFrame, "Cleaned trajectory df"],
+                Annotated[pd.DataFrame, "Cleaned train df"],
+                Annotated[pd.DataFrame, "Cleaned test df"],
+            ],
+        ],
 ) -> Tuple[
     Annotated[pd.DataFrame, "Engineered features"],
     Annotated[pd.Series, "Target variable"],
 ]:
     """Performs feature engineering and prepares data for modeling."""
-
     fe = FeatureEngineering()
     external_information = fe.get_external_information()
-    fe.external_df = pd.DataFrame.from_dict(external_information, orient="index")
+    fe.external_df = pd.DataFrame.from_dict(external_information,
+                                            orient="index")
     fe.external_df.reset_index(inplace=True)
     fe.external_df.rename(columns={"index": "aircraft_type"}, inplace=True)
 
-    fe.train_df = pd.merge(train_df, fe.external_df, on="aircraft_type", how="left")
-    fe.test_df = pd.merge(test_df, fe.external_df, on="aircraft_type", how="left")
+    if config.USE_TRAJECTORY:
+        trajectory_df, train_df, test_df = data
+    else:
+        train_df, test_df = data
+
+    fe.train_df = pd.merge(train_df, fe.external_df, on="aircraft_type",
+                           how="left")
+    fe.test_df = pd.merge(test_df, fe.external_df, on="aircraft_type",
+                          how="left")
 
     aircraft_types_with_info = external_information.keys()
     fe.train_df = fe.train_df[
@@ -34,17 +51,18 @@ def feature_engineering(
     fe.train_df.reset_index(drop=True, inplace=True)
     fe.test_df.reset_index(drop=True, inplace=True)
 
-    td_df = fe.calculate_thrust_minus_drag_for_phases(trajectory_df)
-
-    aggregated_features = fe.aggregate_features(td_df)
-
-    merged_df = pd.merge(
-        fe.train_df,
-        aggregated_features,
-        left_on="flight_id",
-        right_on="flight_id",
-        how="inner",
-    )
+    if config.USE_TRAJECTORY:
+        td_df = fe.calculate_thrust_minus_drag_for_phases(trajectory_df)
+        aggregated_features = fe.aggregate_features(td_df)
+        merged_df = pd.merge(
+            fe.train_df,
+            aggregated_features,
+            left_on="flight_id",
+            right_on="flight_id",
+            how="inner",
+        )
+    else:
+        merged_df = fe.train_df.copy()
 
     exclude_cols = [
         "flight_id",
