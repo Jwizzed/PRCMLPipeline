9341192eeb050d50f06391efa3a0647e1211274c
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-10-13 11:26:41 +0700

    chore: added config dependency injection

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -1,6 +1,7 @@
 from zenml.config import DockerSettings
 from zenml.pipelines import pipeline
 
+from config import Config
 from steps.clean_data import clean_data
 from steps.evaluation import evaluate_models
 from steps.feature_engineering import feature_engineering
@@ -8,17 +9,16 @@ from steps.ingest_data import ingest_data
 from steps.split_data import split_data
 from steps.train_model import train_models
 
-docker_settings = DockerSettings(required_integrations=["catboost"])
+docker_settings = DockerSettings(required_integrations=["MLFLOW"])
 
 
 @pipeline(enable_cache=False, settings={"docker": docker_settings})
 def train_pipeline():
     """Defines the training pipeline."""
-    trajectory_df, train_df, test_df = ingest_data()
-    trajectory_df_cleaned, train_df_cleaned, test_df_cleaned = clean_data(
-        trajectory_df, train_df, test_df
-    )
-    X, y = feature_engineering(trajectory_df_cleaned, train_df_cleaned, test_df_cleaned)
+    config = Config()  # Pipeline interface is not allow to pass unknown params.
+    data = ingest_data(config)
+    cleaned_data = clean_data(config, data)
+    X, y = feature_engineering(config, cleaned_data)
     X_train, X_test, y_train, y_test = split_data(X, y)
     model = train_models(X_train, y_train, X_test, y_test)
     evaluate_models(model, X_test, y_test)
--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -1,22 +1,26 @@
-from zenml import step
+from typing import Tuple, Union
+
 import pandas as pd
-from typing import Tuple
 from typing_extensions import Annotated
+from zenml import step
+from config import Config
 
 
 @step
-def ingest_data() -> Tuple[
-    Annotated[pd.DataFrame, "Trajectory df"],
-    Annotated[pd.DataFrame, "Train df"],
-    Annotated[pd.DataFrame, "Test df"],
+def ingest_data(config: Config) -> Union[
+    Tuple[
+        Annotated[pd.DataFrame, "Train df"],
+        Annotated[pd.DataFrame, "Test df"],
+    ],
+    Tuple[
+        Annotated[pd.DataFrame, "Trajectory df"],
+        Annotated[pd.DataFrame, "Train df"],
+        Annotated[pd.DataFrame, "Test df"],
+    ]
 ]:
-    """Reads the trajectory, train, and test data."""
-    trajectory_file_path = "data/2022-01-01.parquet"
+    """Reads the trajectory, train, and test data based on configuration."""
     challenge_file_path = "data/challenge_set.csv"
     submission_file_path = "data/submission_set.csv"
-
-    trajectory_df = pd.read_parquet(trajectory_file_path)
-    trajectory_df = trajectory_df[: len(trajectory_df) // 10]  # For the experiment
 
     train_df = pd.read_csv(
         challenge_file_path,
@@ -28,4 +32,9 @@ def ingest_data() -> Tuple[
         parse_dates=["date", "actual_offblock_time", "arrival_time"],
     ).drop(["tow"], axis=1)
 
-    return trajectory_df, train_df, test_df
+    if config.USE_TRAJECTORY:
+        trajectory_file_path = "data/2022-01-01.parquet"
+        trajectory_df = pd.read_parquet(trajectory_file_path)
+        return trajectory_df, train_df, test_df
+    else:
+        return train_df, test_df
