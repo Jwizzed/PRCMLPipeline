f0f55b9bde6388671994e6b41d66101c771fd1ab
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-10-17 13:23:15 +0700

    chore: comment the model and update the requirements

--- steps/train_models.py
+++ steps/train_models.py
@@ -6,7 +6,14 @@ import mlflow
 import numpy as np
 import pandas as pd
 from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import \
+    GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
 from xgboost import XGBRegressor
 from zenml import step
 from zenml.client import Client
@@ -39,15 +46,20 @@ def train_models(
         mlflow.log_param("artifact_uri", artifact_uri)
 
     models = {
-        "CatBoost": CatBoostRegressor(
-            iterations=1000, learning_rate=0.1, depth=6, random_state=42
-        ),
-        "XGBoost": XGBRegressor(
-            n_estimators=1000, learning_rate=0.1, max_depth=6, random_state=42
-        ),
-        # "RandomForest": RandomForestRegressor(
-        #     n_estimators=1000, max_depth=6, random_state=42
+        # "CatBoost": CatBoostRegressor(
+        #     iterations=5000, learning_rate=0.1, depth=6, random_state=42
         # ),
+        # "XGBoost": XGBRegressor(
+        #     n_estimators=5000, learning_rate=0.1, max_depth=6, random_state=42
+        # ),
+        # "RandomForest": RandomForestRegressor(
+        #     n_estimators=5000, max_depth=6, random_state=42
+        # ),
+        # "LightGBM": LGBMRegressor(n_estimators=5000, learning_rate=0.1,
+        #                           max_depth=6, random_state=42),
+        # "AdaBoost": AdaBoostRegressor(n_estimators=5000, random_state=42),
+        # "KNN": KNeighborsRegressor(n_neighbors=5)
+        # "Lasso": Lasso(alpha=1.0),
     }
 
     trained_models = {}
