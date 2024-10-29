fd9666274cef0329480735b51ea0a2181b7caf9e
Krittin SETDHAVANICH <krittin.set@ku.th>
2024-10-29 21:56:27 +0700

    feat: added normalize and encode

---
+++ kubeflow/components/data_preprocessing/normalize_data.py
@@ -0,0 +1,32 @@
+from kfp.v2.dsl import component, InputPath, OutputPath
+
+
+
+@component(
+    packages_to_install=['pandas', 'scikit-learn']
+)
+def normalize_dataframe(
+        input_file: InputPath('CSV'),
+        output_file: OutputPath('CSV'),
+        exclude_columns: list,
+        split_by_flown_distance: bool = False
+):
+    """Normalizes the dataframe and excludes specified columns from normalization."""
+    import pandas as pd
+    from sklearn.preprocessing import StandardScaler
+    import numpy as np
+
+    df = pd.read_csv(input_file)
+
+    if split_by_flown_distance:
+        exclude_columns.append('flown_distance')
+
+    df_normalized = df.copy()
+    columns_to_normalize = df.select_dtypes(
+        include=[np.number]).columns.difference(exclude_columns)
+
+    scaler = StandardScaler()
+    df_normalized[columns_to_normalize] = scaler.fit_transform(
+        df[columns_to_normalize])
+
+    df_normalized.to_csv(output_file, index=False)
